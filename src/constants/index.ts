c649b40e8c69f8f6c600fd67b8f28f61557229d1
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2026-01-10 14:49:58 +0100

    Make PORT number instead of string

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,11 +3,12 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const {
-  PORT,
   STATIC_URL,
   IS_DOCKER,
   GOOGLE_RENDER_TIMEOUT,
 } = process.env;
+
+const PORT = Number(process.env.PORT);
 
 const STATIC_FOLDER = '/var/www/static/vizapi';
 
